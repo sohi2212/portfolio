45e540fad398ad5aea5a5ffe33ff3a0917474dc3
Dmitry <sohi02@bk.ru>
2026-01-30 15:14:58 +0300

    upd: Переписал компонент под навигацию

--- src/shared/ui/Nav-item.tsx
+++ src/shared/ui/Nav-item.tsx
@@ -2,10 +2,13 @@ import styles from '../styles/Nav-item.module.css'
 
 interface Props {
     text: string
+    url: string
 }
 
 export const NavItem = (props: Props) => {
     return (
-        <li className={styles.nav__item}>{props.text}</li>
+        <li className={styles.nav__item}>
+            <a href={props.url}>{props.text}</a>
+        </li>
     )
 }
